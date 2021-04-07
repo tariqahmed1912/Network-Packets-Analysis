aafe8220c85c52c0f1855223ae7bb60e9a107515
tariqahmed1912 <tariqahmed1912@gmail.com>
2021-04-07 23:57:05 +0300

    Commit files

--- capture.py
+++ capture.py
@@ -6,6 +6,67 @@ import sys
 import os
 import json
 
+class NetworkPackets():
+    def __init__(self):
+        self.tcp = []
+        self.udp = []
+        self.icmp = []
+        self.arp = []
+        full_path = os.path.realpath(__file__)
+        path, filename = os.path.split(full_path)
+        with open(f"{path}/dump.txt", 'r') as f:
+            self.data = f.read()
+        self.data = self.data.strip()
+        self.data = self.data.replace("\n","@")
+        self.data = self.data.split("@")
+    
+    def gettcp(self):
+        for line in self.data:
+            if 'UDP' in line or 'ICMP' in line or 'ARP' in line:
+                continue
+            else:
+                self.tcp.append(line.split())
+        df_tcp = pd.DataFrame(self.tcp)[[0,1,2,4]]
+        df_tcp.columns = ['timestamp', 'protocol', 'src', 'dest']
+        # df_tcp = df_tcp.to_json(orient='records')
+        # df_tcp = json.loads(df_tcp)
+        return df_tcp
+    
+    def getudp(self):
+        for line in self.data:
+            if 'UDP' in line:
+                self.udp.append(line.split())
+                continue
+        df_udp = pd.DataFrame(self.udp)[[0,2,4,5]]
+        df_udp.columns = ['timestamp', 'src', 'dest', 'protocol']
+        df_udp['protocol'] = df_udp['protocol'].str.replace(',', '')
+        # df_udp = df_udp.to_json(orient='records')
+        # df_udp = json.loads(df_udp)
+        return df_udp
+    
+    def geticmp(self):
+        for line in self.data:
+            if 'ICMP' in line:
+                self.icmp.append(line.split())
+                continue
+        df_icmp = pd.DataFrame(self.icmp)[[0,1,2,4]]
+        df_icmp.columns = ['timestamp', 'protocol', 'src', 'dest']
+        df_icmp['protocol'] = df_icmp['protocol'].str.replace(',', '')
+        # df_icmp = df_icmp.to_json(orient='records')
+        # df_icmp = json.loads(df_icmp)
+        return df_icmp
+
+    def getarp(self):
+        for line in self.data:
+            if 'ARP' in line:
+                self.arp.append(line.split())
+                continue
+        df_arp = pd.DataFrame(self.arp)
+        df_arp[1] = df_arp[1].str.replace(',','')
+        # df_arp = df_arp.to_json(orient='records')
+        # df_arp = json.loads(df_arp)
+        return df_arp
+
 def get_traffic():
     # tcpdump packets stores in Desktop/Extra Project/dump.txt
     tcp, udp, arp, icmp = [],[],[],[]
@@ -38,10 +99,6 @@ def get_traffic():
     df_arp[1] = df_arp[1].str.replace(',','')
     df_icmp['protocol'] = df_icmp['protocol'].str.replace(',', '')
     df_udp['protocol'] = df_udp['protocol'].str.replace(',', '')
-    df = [df_tcp, df_udp, df_arp, df_icmp]
-    for i in range(len(df)):
-        df[i] = df[i].to_json(orient='records')
-        df[i] = json.loads(df[i])
 
     # df_tcp = df_tcp.to_json(orient='records')
     # df_tcp = json.loads(df_tcp)
@@ -54,4 +111,3 @@ def get_traffic():
 
     return df_tcp, df_udp, df_arp, df_icmp
     
-
--- run_app.py
+++ run_app.py
@@ -1,13 +1,59 @@
 """Flask to create website application
 """
 
-from flask import Flask, render_template
-from capture import get_traffic
+from flask import Flask, request, redirect, render_template, url_for
+import pandas as pd
+import plotly
+import re
+from capture import get_traffic, NetworkPackets
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    tcp, udp, arp, icmp = get_traffic()
-    context = {'tcp':tcp, 'udp':udp, 'arp':arp, 'icmp':icmp}
-    return render_template('main.html', context=context)
+@app.route('/', methods=['GET','POST'])
+def main():
+    if request.method == "POST":
+        for protocol in ['tcp','udp','icmp','arp']:
+            if protocol in request.form:
+                break
+        packets = NetworkPackets()
+        if "tcp" in request.form:
+            data = packets.gettcp()
+        elif "udp" in request.form:
+            data = packets.getudp()
+        elif "icmp" in request.form:
+            data = packets.geticmp()
+        else:
+            data = packets.getarp()
+        print(data)
+        graph_data = retrieveFrequencyCount(data)
+        print(graph_data)
+        return render_template('graph.html', graph_data=graph_data)
+    else:
+        tcp, udp, arp, icmp = get_traffic()
+        context = {'tcp':tcp, 'udp':udp, 'arp':arp, 'icmp':icmp}
+        return render_template('main.html', context=context)
+
+
+def retrieveFrequencyCount(df):
+    """Take dataframe as input and retrieve required frequency and count details.
+
+    Args:
+        df (DataFrame): Network packets as a dataframe
+    """
+    re_pattern = re.compile("(\d+\.\d+\.\d+\.\d+)[\.|\:].*")
+    for col in ['src','dest']:
+        df[col] = df[col].str.replace(re_pattern, r'\1')
+    #df['src'] = df['src'].str.replace(re_pattern, '\1')
+    src_ip_list = df.src.value_counts().index.to_list()
+    src_ip_count = df.src.value_counts().to_list()
+    dest_ip_list = df.dest.value_counts().index.to_list()
+    dest_ip_count = df.dest.value_counts().to_list()
+    
+    graph_data = {
+        'src_ip':src_ip_list,
+        'src_count':src_ip_count,
+        'dest_ip':dest_ip_list,
+        'dest_count':dest_ip_count
+    }
+    
+    return graph_data
